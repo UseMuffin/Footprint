0314ee2635522210e7b8ce4f4681469d6f6a6399
Jad Bitar <jadbitar@mac.com>
2016-02-27 14:38:36 -0500

    Normalize at an earlier stage

--- src/Model/Behavior/FootprintBehavior.php
+++ src/Model/Behavior/FootprintBehavior.php
@@ -41,7 +41,7 @@ class FootprintBehavior extends Behavior
         $config = $this->config();
 
         foreach ($config['events'] as $name => $options) {
-            $this->config('events.' . $name, Hash::normalize($options));
+            $this->config('events.' . $name, Hash::normalize((array)$options));
             foreach (array_keys($this->config('events.' . $name)) as $field) {
                 if (!in_array($field, $config['propertiesMap']) && !isset($config['propertiesMap'][$field])) {
                     $config['propertiesMap'][] = $field;
@@ -76,7 +76,7 @@ class FootprintBehavior extends Behavior
     public function beforeFind(Event $event, Query $query, ArrayObject $options)
     {
         $eventName = $event->name();
-        $config = Hash::normalize((array)$this->config('events.' . $eventName));
+        $config = $this->config('events.' . $eventName);
 
         foreach (array_keys($config) as $field) {
             $path = $this->config('propertiesMap.' . $field);
