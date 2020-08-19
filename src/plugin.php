8eff8c186c6a6e474c8da899c67cc756f4ff9bd1
Jacob Tyler <JacobAGTyler@users.noreply.github.com>
2020-08-19 14:29:01 +0100

    Add Plugin Manifest
    This should enable the correct loading of the plugin, long term and to preempt the warning coming in 4.2. See discussion on CakePHP/CakePHP 4.1.2 bug fix in the long term. https://github.com/cakephp/cakephp/pull/14891

---
+++ src/plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Muffin\Footprint;
+
+use Cake\Core\BasePlugin;
+use Cake\Routing\RouteBuilder;
+
+/**
+ * Plugin for Expose
+ */
+class Plugin extends BasePlugin {
+
+	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $bootstrapEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected routesEnabled = false;
+
+}
