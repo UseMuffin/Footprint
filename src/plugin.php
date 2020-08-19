80bd0912449935943ffb5e07fbff2f9d2dc5fdbe
jacobAGTyler <jacobagtyler@users.noreply.github.com>
2020-08-19 16:26:39 +0100

    PHPBF Fixes

--- src/plugin.php
+++ src/plugin.php
@@ -8,21 +8,21 @@ use Cake\Routing\RouteBuilder;
 /**
  * Plugin for Expose
  */
-class Plugin extends BasePlugin {
+class Plugin extends BasePlugin
+{
 
-	/**
-	 * @var bool
-	 */
-	protected $middlewareEnabled = false;
+    /**
+     * @var bool
+     */
+    protected $middlewareEnabled = false;
 
-	/**
-	 * @var bool
-	 */
-	protected $bootstrapEnabled = false;
+    /**
+     * @var bool
+     */
+    protected $bootstrapEnabled = false;
 
-	/**
-	 * @var bool
-	 */
-	protected $routesEnabled = false;
-
+    /**
+     * @var bool
+     */
+    protected $routesEnabled = false;
 }
