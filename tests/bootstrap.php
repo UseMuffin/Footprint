d22e65f0641f12d7830e96933218304b44b6b649
ADmad <admad.coder@gmail.com>
2026-04-22 11:26:28 +0530

    Cleanup tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use Cake\Core\Configure;
-
 /**
  * Test suite bootstrap.
  *
@@ -32,7 +30,3 @@ if (file_exists($root . '/config/bootstrap.php')) {
 }
 
 require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
-
-Configure::write('Error.ignoredDeprecationPaths', [
-    'src/TestSuite/Fixture/FixtureInjector.php',
-]);
